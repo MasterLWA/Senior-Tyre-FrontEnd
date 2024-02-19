147915d4fd2eedb5ee847c8bafcb256a6fdc9dda
Lakindu Widuranga Alwis <vidurangalakindu@gmail.com>
2024-02-20 02:01:41 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-
-// export const ENDPOINT = 'https://tyre-backend.onrender.com';
+export const ENDPOINT = 'https://tyre-backend.onrender.com';
 
 // export const ENDPOINT = 'http://localhost:5006';
-export const ENDPOINT = 'http://154.56.63.83:5006';
+// export const ENDPOINT = 'http://154.56.63.83:5006';
